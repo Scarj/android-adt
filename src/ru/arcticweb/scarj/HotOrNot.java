02fa2f1776dd2dfebf2d24ba9ad99a72f2bff7f0
Евгений <scarj@arcticmedia.ru>
2012-07-19 16:38:32 +0400

    lesson 113

--- src/ru/arcticweb/scarj/HotOrNot.java
+++ src/ru/arcticweb/scarj/HotOrNot.java
@@ -1,5 +1,10 @@
 package ru.arcticweb.scarj;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
 public class HotOrNot {
 	
 	public static final String KEY_ROWID = "id";
@@ -9,5 +14,33 @@ public class HotOrNot {
 	private static final String DB_NAME = "HotOrNot";
 	private static final String DB_TABLE = "peoples";
 	private static final int DB_VERSION = 1;
+	
+	private DbHelper ourHelper;
+	private final Context ourContext;
+	private SQLiteDatabase ourDatabase;
+	
+	private static class DbHelper extends SQLiteOpenHelper {
+
+		public DbHelper(Context context) {
+			super(context, DB_NAME, null, DB_VERSION);
+			// TODO Auto-generated constructor stub
+		}
+
+		@Override
+		public void onCreate(SQLiteDatabase db) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+			// TODO Auto-generated method stub
+			
+		}
+	}
+	
+	public HotOrNot(Context c) {
+		ourContext = c;		
+	}
 
 }
