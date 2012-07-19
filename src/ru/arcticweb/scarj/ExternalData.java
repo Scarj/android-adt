c6c564faadaac9dd9473f455f27f0b8f0e23d607
Евгений <scarj@arcticmedia.ru>
2012-07-19 14:32:15 +0400

    lesson 105-106

--- src/ru/arcticweb/scarj/ExternalData.java
+++ src/ru/arcticweb/scarj/ExternalData.java
@@ -1,14 +1,25 @@
 package ru.arcticweb.scarj;
 
+import java.io.File;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Environment;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-public class ExternalData extends Activity{
+public class ExternalData extends Activity implements OnItemSelectedListener{
 	
 	private TextView canWrite, canRead;
 	private String state;
+	boolean canW, canR;
+	Spinner spinner;
+	String[] paths = {"Music", "Pictures", "Downloads"};
+	File path = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +34,51 @@ public class ExternalData extends Activity{
 		if(state.equals(Environment.MEDIA_MOUNTED)){
 			// read&write
 			canWrite.setText("true");
-			canRead.setText("true");			
+			canRead.setText("true");
+			canR = canW = true;
 		} else if(state.equals(Environment.MEDIA_MOUNTED_READ_ONLY)) {
 			// read&!write
 			canWrite.setText("false");
 			canRead.setText("true");
+			canR = true; canW = false;
 		} else {
 			canWrite.setText("false");
 			canRead.setText("false");
+			canR = false; canW = true;
 		}
+		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(ExternalData.this, android.R.layout.simple_spinner_item,paths);
+		
+		spinner = (Spinner) findViewById(R.id.spinner1);
+		spinner.setAdapter(adapter);
+		spinner.setOnItemSelectedListener(this);
+	}
+
+	@Override
+	public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
+			long arg3) {
+		// TODO Auto-generated method stub
+		int position = spinner.getSelectedItemPosition();
+		switch (position) {
+		case 0:
+			path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC);
+			break;
+
+		case 1:
+			path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+			break;
+
+		case 2:
+			path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+			break;			
+		}
+		
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
