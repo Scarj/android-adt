a07f548d5df4f5683036da89c42707866412a121
Евгений <scarj@arcticmedia.ru>
2012-07-19 16:26:26 +0400

    lesson 112

--- src/ru/arcticweb/scarj/HotOrNot.java
+++ src/ru/arcticweb/scarj/HotOrNot.java
@@ -1,5 +1,13 @@
 package ru.arcticweb.scarj;
 
 public class HotOrNot {
+	
+	public static final String KEY_ROWID = "id";
+	public static final String KEY_NAME = "name";
+	public static final String KEY_HOTNESS = "hotness";
+	
+	private static final String DB_NAME = "HotOrNot";
+	private static final String DB_TABLE = "peoples";
+	private static final int DB_VERSION = 1;
 
 }
